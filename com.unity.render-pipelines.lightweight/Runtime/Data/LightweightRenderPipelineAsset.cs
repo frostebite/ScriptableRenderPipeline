516c8f01e9d21ddd78fb3161d289feca9b63ea9d
DocWorks-Preview <docworkspreview@gmail.com>
2019-03-25 16:05:30 +0100

    DW CMS Message

--- com.unity.render-pipelines.lightweight/Runtime/Data/LightweightRenderPipelineAsset.cs
+++ com.unity.render-pipelines.lightweight/Runtime/Data/LightweightRenderPipelineAsset.cs
@@ -28,12 +28,18 @@ namespace UnityEngine.Rendering.LWRP
         SoftShadows,
     }
 
+    
     public enum ShadowResolution
     {
+        
         _256 = 256,
+        
         _512 = 512,
+        
         _1024 = 1024,
+        
         _2048 = 2048,
+        
         _4096 = 4096
     }
 
