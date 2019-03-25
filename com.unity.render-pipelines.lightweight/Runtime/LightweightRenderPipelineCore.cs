853acfbc021842077c8ef92321f8641f9a9a8096
DocWorks-Preview <docworkspreview@gmail.com>
2019-03-25 16:05:24 +0100

    DW CMS Message

--- com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipelineCore.cs
+++ com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipelineCore.cs
@@ -84,19 +84,32 @@ namespace UnityEngine.Rendering.LWRP
         public IEnumerator<Action<RenderTargetIdentifier, CommandBuffer> > captureActions;
     }
 
+    
     public struct ShadowData
     {
+        
         public bool supportsMainLightShadows;
+        
         public bool requiresScreenSpaceShadowResolve;
+        
         public int mainLightShadowmapWidth;
+        
         public int mainLightShadowmapHeight;
+        
         public int mainLightShadowCascadesCount;
+        
         public Vector3 mainLightShadowCascadesSplit;
+        
         public bool supportsAdditionalLightShadows;
+        
         public int additionalLightsShadowmapWidth;
+        
         public int additionalLightsShadowmapHeight;
+        
         public bool supportsSoftShadows;
+        
         public int shadowmapDepthBufferBits;
+        
         public List<Vector4> bias;
     }
 
