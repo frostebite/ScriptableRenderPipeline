5141ded4ca2033750a93be9a3ab4c331975dde5c
DocWorks-Preview <docworkspreview@gmail.com>
2019-03-25 16:05:13 +0100

    DW CMS Message

--- com.unity.render-pipelines.lightweight/Editor/ShaderGUI/BaseShaderGUI.cs
+++ com.unity.render-pipelines.lightweight/Editor/ShaderGUI/BaseShaderGUI.cs
@@ -16,11 +16,16 @@ namespace UnityEditor
             Transparent
         }
 
+        
         public enum BlendMode
         {
+            
             Alpha,   // Old school alpha-blending mode, fresnel does not affect amount of transparency
+            
             Premultiply, // Physically plausible transparency mode, implemented as alpha pre-multiply
+            
             Additive,
+            
             Multiply
         }
 
