5b48f90e202cf0b958e8110dcae8511a68c864f8
DocWorks-Preview <docworkspreview@gmail.com>
2019-03-25 16:05:17 +0100

    DW CMS Message

--- com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipelineCore.cs
+++ com.unity.render-pipelines.lightweight/Runtime/LightweightRenderPipelineCore.cs
@@ -23,13 +23,20 @@ namespace UnityEngine.Rendering.LWRP
         public bool killAlphaInFinalBlit;
     }
 
+    
     public struct LightData
     {
+        
         public int mainLightIndex;
+        
         public int additionalLightsCount;
+        
         public int maxPerObjectAdditionalLightsCount;
+        
         public NativeArray<VisibleLight> visibleLights;
+        
         public bool shadeAdditionalLightsPerVertex;
+        
         public bool supportsMixedLighting;
     }
 
