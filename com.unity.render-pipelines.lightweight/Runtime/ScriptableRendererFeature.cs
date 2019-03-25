538b1a826fb188522b869c692cf4487899620115
DocWorks-Preview <docworkspreview@gmail.com>
2019-03-25 16:05:21 +0100

    DW CMS Message

--- com.unity.render-pipelines.lightweight/Runtime/ScriptableRendererFeature.cs
+++ com.unity.render-pipelines.lightweight/Runtime/ScriptableRendererFeature.cs
@@ -1,22 +1,15 @@
 namespace UnityEngine.Rendering.LWRP
 {
-    /// <summary>
-    /// Class <c>ScriptableRendererFeature</c> extends <c>ScriptableRenderer</c> with additional features.
-    /// </summary>
-    /// <seealso cref="ScriptableRenderer"/>
-    public abstract class ScriptableRendererFeature : ScriptableObject
+    /// <!-- Badly formed XML comment ignored for member "T:UnityEngine.Rendering.LWRP.ScriptableRendererFeature" -->
+            public abstract class ScriptableRendererFeature : ScriptableObject
     {
-        /// <summary>
-        /// Initializes this feature's resources.
-        /// </summary>
-        public abstract void Create();
+        ///     <summary>
+                ///     Initializes this feature's resources.
+                ///     </summary>
+                        public abstract void Create();
 
-        /// <summary>
-        /// Injects one or multiple <c>ScriptableRenderPass</c> in the renderer.
-        /// </summary>
-        /// <param name="renderPasses">List of render passes to add to.</param>
-        /// <param name="renderingData">Rendering state. Use this to setup render passes.</param>
-        public abstract void AddRenderPasses(ScriptableRenderer renderer,
+        /// <!-- Badly formed XML comment ignored for member "M:UnityEngine.Rendering.LWRP.ScriptableRendererFeature.AddRenderPasses(UnityEngine.Rendering.LWRP.ScriptableRenderer,UnityEngine.Rendering.LWRP.RenderingData@)" -->
+                        public abstract void AddRenderPasses(ScriptableRenderer renderer,
             ref RenderingData renderingData);
 
         void OnEnable()
